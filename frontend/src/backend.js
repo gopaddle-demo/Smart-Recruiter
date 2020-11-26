c6076edeefe0e4b3d26736a572c07a577ebaccf4
nayanpal777 <nayanpalmns@gmail.com>
2020-11-26 20:40:48 +0530

    updated backend file

--- frontend/src/backend.js
+++ frontend/src/backend.js
@@ -1,2 +1,2 @@
-export const API = `http://localhost:8090/`;
-//export const API = `https://smart-recruiter.herokuapp.com/`;
+//export const API = `http://localhost:8090/`;
+export const API = `https://smart-recruiter.herokuapp.com/`;
